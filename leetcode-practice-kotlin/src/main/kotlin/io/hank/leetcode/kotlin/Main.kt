41840be2ded7dcb885a5159e1dad7e06c77880d9
Hao Liu <hliu4@opentext.com>
2024-12-06 18:12:41 +0800

    prac: add Kotlin implementation

---
+++ leetcode-practice-kotlin/src/main/kotlin/io/hank/leetcode/kotlin/Main.kt
@@ -0,0 +1,11 @@
+package io.hank.leetcode.kotlin
+
+import io.hank.leetcode.kotlin.practices._001_Two_Sum
+
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Hello, Leetcode!")
+        _001_Two_Sum().execute()
+    }
+}
--- leetcode-practice-typescript/index.ts
+++ leetcode-practice-typescript/index.ts
@@ -1,3 +1,4 @@
 import {execute} from './src/_002_Add_Two_Numbers';
 
+console.log('Hello, Leetcode!')
 execute();
