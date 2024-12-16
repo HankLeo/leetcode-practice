772310cdec8b3fe828a24ba2c4dc1025599255d0
Hao Liu <hliu4@opentext.com>
2024-12-17 00:46:26 +0800

    prac: java of problem 5 - Longest Palindromic Substring

--- leetcode-practice-java/src/main/java/io/hank/leetcode/Main.java
+++ leetcode-practice-java/src/main/java/io/hank/leetcode/Main.java
@@ -1,9 +1,6 @@
 package io.hank.leetcode;
 
-import io.hank.leetcode.practices._001_Two_Sum;
-import io.hank.leetcode.practices._002_Add_Two_Numbers;
-import io.hank.leetcode.practices._003_Longest_Substring_Without_Repeating;
-import io.hank.leetcode.practices._004_Median_of_Two_Sorted_Arrays;
+import io.hank.leetcode.practices.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,5 +9,6 @@ public class Main {
         new _002_Add_Two_Numbers().execute();
         new _003_Longest_Substring_Without_Repeating().execute();
         new _004_Median_of_Two_Sorted_Arrays().execute();
+        new _005_Longest_Palindromic_Substring().execute();
     }
 }
