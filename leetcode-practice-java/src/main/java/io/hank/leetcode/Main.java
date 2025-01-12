7e8f86e4d3e58403a642e1b1507f0255443f2708
Hao Liu <hliu4@opentext.com>
2025-01-12 22:54:00 +0800

    prac: java for _088_Merge_Sorted_Array

--- leetcode-practice-java/src/main/java/io/hank/leetcode/Main.java
+++ leetcode-practice-java/src/main/java/io/hank/leetcode/Main.java
@@ -22,6 +22,7 @@ public class Main {
         new _014_Longest_Common_Prefix().execute();
         new _015_3Sum().execute();
         new _016_3Sum_Closest().execute();
+        new _088_Merge_Sorted_Array().execute();
         new _167_Two_Sum_II_SortedArray().execute();
         new _345_Reverse_Vowels_of_String().execute();
         new _633_Sum_of_Square_Numbers().execute();
