7ad18d1347f9630e773963833c0c6c3f8672f8f7
Hao Liu <hliu4@opentext.com>
2025-01-19 00:32:59 +0800

    prac: java for _435_Nonoverlapping_Intervals

--- leetcode-practice-java/src/main/java/io/hank/leetcode/Main.java
+++ leetcode-practice-java/src/main/java/io/hank/leetcode/Main.java
@@ -29,6 +29,7 @@ public class Main {
         new _215_Kth_Largest_Element_in_Array().execute();
         new _345_Reverse_Vowels_of_String().execute();
         new _347_TopK_Frequent_Elements().execute();
+        new _435_Nonoverlapping_Intervals().execute();
         new _451_Sort_Characters_By_Frequency().execute();
         new _455_Assign_Cookies().execute();
         new _524_Longest_Word_in_Dictionary_through_Deleting().execute();
---
+++ leetcode-practice-java/src/main/java/io/hank/leetcode/practices/_435_Nonoverlapping_Intervals.java
@@ -0,0 +1,62 @@
+package io.hank.leetcode.practices;
+
+import java.util.Arrays;
+
+import io.hank.leetcode.complexity.ComplexityType;
+import io.hank.leetcode.complexity.SpaceComplexity;
+import io.hank.leetcode.complexity.TimeComplexity;
+
+/**
+ * <pre>
+ * Given an array of intervals `intervals` where intervals[i] = [start_i, end_i], return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
+ *
+ * Note that intervals which only touch at a point are non-overlapping. For example, [1, 2] and [2, 3] are non-overlapping.
+ *
+ *
+ * Example 1:
+ *
+ * Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
+ * Output: 1
+ * Explanation: [1,3] can be removed and the rest of the intervals are non-overlapping.
+ * Example 2:
+ *
+ * Input: intervals = [[1,2],[1,2],[1,2]]
+ * Output: 2
+ * Explanation: You need to remove two [1,2] to make the rest of the intervals non-overlapping.
+ * Example 3:
+ *
+ * Input: intervals = [[1,2],[2,3]]
+ * Output: 0
+ * Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= intervals.length <= 10^5
+ * intervals[i].length == 2
+ * -5 * 10^4 <= start_i < end_i <= 5 * 10^4
+ * </pre>
+ */
+public class _435_Nonoverlapping_Intervals extends LeetcodeProblemSolution {
+    @TimeComplexity(ComplexityType.O_N_LOG_N)
+    @SpaceComplexity(ComplexityType.O_N)
+    int eraseOverlapIntervals(int[][] intervals) {
+        int len = intervals.length;
+        if (len <= 1) {
+            return 0;
+        }
+
+        // sort by the end value of interval
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]); // lambda is faster than Comparator
+        // count the non-overlapping intervals
+        int count = 1;
+        int end = intervals[0][1];
+        for (int i = 1; i < len; i++) {
+            if (intervals[i][0] >= end) {
+                count++;
+                end = intervals[i][1];
+            }
+        }
+        return len - count;
+    }
+}
