ac29ad100c8ce232dd12d2f34d482f37f6cd3b80
Hao Liu <hliu4@opentext.com>
2025-02-23 23:21:19 +0800

    prac: java for _127_Word_Ladder

--- leetcode-practice-java/src/main/java/io/hank/leetcode/Main.java
+++ leetcode-practice-java/src/main/java/io/hank/leetcode/Main.java
@@ -5,7 +5,12 @@ import io.hank.leetcode.practices.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Leetcode!");
-
+        runTwoPointer();
+        runSort();
+        runGreedy();
+        runBinarySearch();
+        runRecursion();
+        runSearch();
 //        new _001_Two_Sum().execute();
 //        new _002_Add_Two_Numbers().execute();
 //        new _003_Longest_Substring_Without_Repeating().execute();
@@ -22,37 +27,95 @@ public class Main {
 //        new _014_Longest_Common_Prefix().execute();
 //        new _015_3Sum().execute();
 //        new _016_3Sum_Closest().execute();
-        new _034_Find_First_Last_Element_in_Sorted_Array().execute();
-        new _053_Maximum_Subarray().execute();
-        new _069_Sqrt_X().execute();
-        new _075_Sort_Colors().execute();
+//        new _034_Find_First_Last_Element_in_Sorted_Array().execute();
+//        new _053_Maximum_Subarray().execute();
+//        new _069_Sqrt_X().execute();
+//        new _075_Sort_Colors().execute();
+//        new _088_Merge_Sorted_Array().execute();
+//        new _095_Unique_Binary_Search_Trees_II().execute();
+//        new _121_Best_Time_to_Buy_and_Sell_Stock().execute();
+//        new _122_Best_Time_to_Buy_and_Sell_Stock_II().execute();
+//        new _127_Word_Ladder().execute();
+//        new _141_Linked_List_Cycle().execute();
+//        new _153_Find_Minimum_in_Rotated_Sorted_Array().execute();
+//        new _167_Two_Sum_II_SortedArray().execute();
+//        new _215_Kth_Largest_Element_in_Array().execute();
+//        new _241_Different_Ways_Add_Parentheses().execute();
+//        new _278_First_Bad_Version().execute();
+//        new _279_Perfect_Squares().execute();
+//        new _345_Reverse_Vowels_of_String().execute();
+//        new _347_TopK_Frequent_Elements().execute();
+//        new _392_Is_Subsequence().execute();
+//        new _406_Queue_Reconstruction_by_Height().execute();
+//        new _435_Nonoverlapping_Intervals().execute();
+//        new _451_Sort_Characters_By_Frequency().execute();
+//        new _452_Min_Arrows_to_Burst_Balloons().execute();
+//        new _455_Assign_Cookies().execute();
+//        new _524_Longest_Word_in_Dictionary_through_Deleting().execute();
+//        new _540_Single_Element_in_Sorted_Array().execute();
+//        new _605_Can_Place_Flowers().execute();
+//        new _633_Sum_of_Square_Numbers().execute();
+//        new _665_Non_decreasing_Array().execute();
+//        new _680_Valid_Palindrome_II().execute();
+//        new _744_Find_Smallest_Letter_GT_Target().execute();
+//        new _763_Partition_Labels().execute();
+//        new _1091_Shortest_Path_in_Binary_Matrix().execute();
+    }
+
+    private static void runTwoPointer() {
+        System.out.println("\n******** Two Pointer *******");
+        new _167_Two_Sum_II_SortedArray().execute();
+        new _633_Sum_of_Square_Numbers().execute();
+        new _345_Reverse_Vowels_of_String().execute();
+        new _680_Valid_Palindrome_II().execute();
         new _088_Merge_Sorted_Array().execute();
-        new _095_Unique_Binary_Search_Trees_II().execute();
+        new _141_Linked_List_Cycle().execute();
+        new _524_Longest_Word_in_Dictionary_through_Deleting().execute();
+    }
+
+    private static void runSort() {
+        System.out.println("\n******** Sort *******");
+        new _215_Kth_Largest_Element_in_Array().execute();
+        new _347_TopK_Frequent_Elements().execute();
+        new _451_Sort_Characters_By_Frequency().execute();
+        new _075_Sort_Colors().execute();
+    }
+
+    private static void runGreedy() {
+        System.out.println("\n******** Greedy *******");
+        new _455_Assign_Cookies().execute();
+        new _435_Nonoverlapping_Intervals().execute();
+        new _452_Min_Arrows_to_Burst_Balloons().execute();
+        new _406_Queue_Reconstruction_by_Height().execute();
         new _121_Best_Time_to_Buy_and_Sell_Stock().execute();
         new _122_Best_Time_to_Buy_and_Sell_Stock_II().execute();
-        new _141_Linked_List_Cycle().execute();
-        new _153_Find_Minimum_in_Rotated_Sorted_Array().execute();
-        new _167_Two_Sum_II_SortedArray().execute();
-        new _215_Kth_Largest_Element_in_Array().execute();
-        new _241_Different_Ways_Add_Parentheses().execute();
-        new _278_First_Bad_Version().execute();
-        new _279_Perfect_Squares().execute();
-        new _345_Reverse_Vowels_of_String().execute();
-        new _347_TopK_Frequent_Elements().execute();
-        new _392_Is_Subsequence().execute();
-        new _406_Queue_Reconstruction_by_Height().execute();
-        new _435_Nonoverlapping_Intervals().execute();
-        new _451_Sort_Characters_By_Frequency().execute();
-        new _452_Min_Arrows_to_Burst_Balloons().execute();
-        new _455_Assign_Cookies().execute();
-        new _524_Longest_Word_in_Dictionary_through_Deleting().execute();
-        new _540_Single_Element_in_Sorted_Array().execute();
         new _605_Can_Place_Flowers().execute();
-        new _633_Sum_of_Square_Numbers().execute();
+        new _392_Is_Subsequence().execute();
         new _665_Non_decreasing_Array().execute();
-        new _680_Valid_Palindrome_II().execute();
-        new _744_Find_Smallest_Letter_GT_Target().execute();
+        new _053_Maximum_Subarray().execute();
         new _763_Partition_Labels().execute();
+    }
+
+    private static void runBinarySearch() {
+        System.out.println("\n******** Binary Search *******");
+        new _069_Sqrt_X().execute();
+        new _744_Find_Smallest_Letter_GT_Target().execute();
+        new _540_Single_Element_in_Sorted_Array().execute();
+        new _278_First_Bad_Version().execute();
+        new _015_3Sum().execute();
+        new _034_Find_First_Last_Element_in_Sorted_Array().execute();
+    }
+
+    private static void runRecursion() {
+        System.out.println("\n******** Recursion *******");
+        new _241_Different_Ways_Add_Parentheses().execute();
+        new _095_Unique_Binary_Search_Trees_II().execute();
+    }
+
+    private static void runSearch() {
+        System.out.println("\n******** Search *******");
         new _1091_Shortest_Path_in_Binary_Matrix().execute();
+        new _279_Perfect_Squares().execute();
+        new _127_Word_Ladder().execute();
     }
 }
---
+++ leetcode-practice-java/src/main/java/io/hank/leetcode/practices/_127_Word_Ladder.java
@@ -0,0 +1,96 @@
+package io.hank.leetcode.practices;
+
+import io.hank.leetcode.annotations.*;
+
+import java.util.*;
+
+/**
+ * <pre>
+ * A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
+ *
+ * Every adjacent pair of words differs by a single letter.
+ * Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+ * sk == endWord
+ * Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+ * Output: 5
+ * Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
+ * Example 2:
+ *
+ * Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+ * Output: 0
+ * Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= beginWord.length <= 10
+ * endWord.length == beginWord.length
+ * 1 <= wordList.length <= 5000
+ * wordList[i].length == beginWord.length
+ * beginWord, endWord, and wordList[i] consist of lowercase English letters.
+ * beginWord != endWord
+ * All the words in wordList are unique.
+ * </pre>
+ */
+public class _127_Word_Ladder extends LeetcodeProblemSolution {
+
+    @Topic(TopicType.BFS)
+    @TimeComplexity(ComplexityType.O_MN)
+    @SpaceComplexity(ComplexityType.O_N)
+    int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if (!wordList.contains(endWord)) {
+            return 0;
+        }
+
+        Queue<String> queue = new LinkedList<>();
+        queue.add(beginWord);
+        int level = 1;
+        Set<String> words = new HashSet<>(wordList);
+
+        while (!queue.isEmpty()) {
+            level++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String cur = queue.poll();
+                // loop all connected strings
+                for (int j = 0; j < cur.length(); j++) {
+                    char[] chars = cur.toCharArray();
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (chars[j] == c) {
+                            continue;
+                        }
+                        chars[j] = c;
+                        String next = new String(chars);
+                        if (words.contains(next)) {
+                            if (next.equals(endWord)) {
+                                return level;
+                            }
+                            queue.add(next);
+                            words.remove(next);
+                        }
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+    private boolean isConnected(String parent, String child) {
+        int diff = 0;
+        for (int i = 0; i < parent.length(); i++) {
+            if (diff > 1) {
+                return false;
+            }
+            if (parent.charAt(i) != child.charAt(i)) {
+                diff++;
+            }
+        }
+        return diff <= 1;
+    }
+}
