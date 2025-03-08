f8c8ccb529261ae5f667d9a23cc987bf1aec22ee
Hao Liu <hliu4@opentext.com>
2025-03-08 12:42:53 +0800

    prac: java for _079_Word_Search

--- leetcode-practice-java/src/main/java/io/hank/leetcode/Main.java
+++ leetcode-practice-java/src/main/java/io/hank/leetcode/Main.java
@@ -12,55 +12,6 @@ public class Main {
         runRecursion();
         runSearch();
         runBacktrack();
-//        new _001_Two_Sum().execute();
-//        new _002_Add_Two_Numbers().execute();
-//        new _003_Longest_Substring_Without_Repeating().execute();
-//        new _004_Median_of_Two_Sorted_Arrays().execute();
-//        new _005_Longest_Palindromic_Substring().execute();
-//        new _006_Zigzag_Conversion().execute();
-//        new _007_Reverse_Integer().execute();
-//        new _008_String_To_Integer().execute();
-//        new _009_Palindrome_Number().execute();
-//        new _010_Regular_Expression_Matching().execute();
-//        new _011_Container_With_Most_Water().execute();
-//        new _012_Integer_to_Roman().execute();
-//        new _013_Roman_to_Integer().execute();
-//        new _014_Longest_Common_Prefix().execute();
-//        new _015_3Sum().execute();
-//        new _016_3Sum_Closest().execute();
-//        new _034_Find_First_Last_Element_in_Sorted_Array().execute();
-//        new _053_Maximum_Subarray().execute();
-//        new _069_Sqrt_X().execute();
-//        new _075_Sort_Colors().execute();
-//        new _088_Merge_Sorted_Array().execute();
-//        new _095_Unique_Binary_Search_Trees_II().execute();
-//        new _121_Best_Time_to_Buy_and_Sell_Stock().execute();
-//        new _122_Best_Time_to_Buy_and_Sell_Stock_II().execute();
-//        new _127_Word_Ladder().execute();
-//        new _141_Linked_List_Cycle().execute();
-//        new _153_Find_Minimum_in_Rotated_Sorted_Array().execute();
-//        new _167_Two_Sum_II_SortedArray().execute();
-//        new _215_Kth_Largest_Element_in_Array().execute();
-//        new _241_Different_Ways_Add_Parentheses().execute();
-//        new _278_First_Bad_Version().execute();
-//        new _279_Perfect_Squares().execute();
-//        new _345_Reverse_Vowels_of_String().execute();
-//        new _347_TopK_Frequent_Elements().execute();
-//        new _392_Is_Subsequence().execute();
-//        new _406_Queue_Reconstruction_by_Height().execute();
-//        new _435_Nonoverlapping_Intervals().execute();
-//        new _451_Sort_Characters_By_Frequency().execute();
-//        new _452_Min_Arrows_to_Burst_Balloons().execute();
-//        new _455_Assign_Cookies().execute();
-//        new _524_Longest_Word_in_Dictionary_through_Deleting().execute();
-//        new _540_Single_Element_in_Sorted_Array().execute();
-//        new _605_Can_Place_Flowers().execute();
-//        new _633_Sum_of_Square_Numbers().execute();
-//        new _665_Non_decreasing_Array().execute();
-//        new _680_Valid_Palindrome_II().execute();
-//        new _744_Find_Smallest_Letter_GT_Target().execute();
-//        new _763_Partition_Labels().execute();
-//        new _1091_Shortest_Path_in_Binary_Matrix().execute();
     }
 
     private static void runTwoPointer() {
@@ -129,5 +80,6 @@ public class Main {
         System.out.println("\n******** Backtrack *******");
         new _017_Letter_Combinations_of_Phone_Number().execute();
         new _093_Restore_IP_Addresses().execute();
+        new _079_Word_Search().execute();
     }
 }
