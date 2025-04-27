9c97f3b81fdec3485aca901542d203d3a123b77c
Hao Liu <hliu4@opentext.com>
2025-04-27 21:40:06 +0800

    prac: java for problem _56_Merge_Intervals

--- leetcode-practice-java/src/main/java/io/hank/leetcode/practices/arraymatrix/ArrayMatrixPractices.java
+++ leetcode-practice-java/src/main/java/io/hank/leetcode/practices/arraymatrix/ArrayMatrixPractices.java
@@ -3,6 +3,7 @@ package io.hank.leetcode.practices.arraymatrix;
 public class ArrayMatrixPractices {
     public static void execute() {
         System.out.println("\n******** Array & Matrix ********");
+        new _56_Merge_Intervals().execute();
         new _283_Move_Zeroes().execute();
         new _566_Reshape_the_Matrix().execute();
         new _485_Max_Consecutive_Ones().execute();
