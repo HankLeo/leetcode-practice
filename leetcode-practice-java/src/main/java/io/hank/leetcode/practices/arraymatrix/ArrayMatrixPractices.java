a94734c2a6b60b525f4c7e57bbfcd878df3ec2f7
Hao Liu <hliu4@opentext.com>
2025-04-30 16:56:06 +0800

    prac: java for problem _49_Group_Anagrams

--- leetcode-practice-java/src/main/java/io/hank/leetcode/practices/arraymatrix/ArrayMatrixPractices.java
+++ leetcode-practice-java/src/main/java/io/hank/leetcode/practices/arraymatrix/ArrayMatrixPractices.java
@@ -5,6 +5,7 @@ public class ArrayMatrixPractices {
         System.out.println("\n******** Array & Matrix ********");
         new _56_Merge_Intervals().execute();
         new _73_Set_Matrix_Zeroes().execute();
+        new _189_Rotate_Array().execute();
         new _283_Move_Zeroes().execute();
         new _566_Reshape_the_Matrix().execute();
         new _485_Max_Consecutive_Ones().execute();
