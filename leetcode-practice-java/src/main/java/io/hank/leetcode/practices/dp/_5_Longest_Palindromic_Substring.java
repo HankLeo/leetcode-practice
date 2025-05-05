81ab82c1b7742518332d44ee51bb656b56001fe6
Hao Liu <hliu4@opentext.com>
2025-05-05 13:02:21 +0800

    prac: java for problem longestPalindrome

--- leetcode-practice-java/src/main/java/io/hank/leetcode/practices/dp/_5_Longest_Palindromic_Substring.java
+++ leetcode-practice-java/src/main/java/io/hank/leetcode/practices/dp/_5_Longest_Palindromic_Substring.java
@@ -1,84 +1,83 @@
 package io.hank.leetcode.practices.dp;
 
-import io.hank.leetcode.annotations.Topic;
-import io.hank.leetcode.annotations.TopicType;
+import io.hank.leetcode.annotations.*;
 import io.hank.leetcode.practices.LeetcodeProblemSolution;
 
 /**
- * Given a string s, return the longest palindromic substring in s.
- * <p>
- * Example 1:
- * <p>
- * Input: s = "babad"
- * <p>
- * Output: "bab"
- * <p>
- * Explanation: "aba" is also a valid answer.
- * <p>
- * Example 2:
- * <p>
- * Input: s = "cbbd"
- * <p>
- * Output: "bb"
- * <p>
- * Constraints:
- * <p>
+ * <pre>
+ * 给你一个字符串 s，找到 s 中最长的 回文 子串。
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：s = "babad"
+ * 输出："bab"
+ * 解释："aba" 同样是符合题意的答案。
+ *
+ * 示例 2：
+ *
+ * 输入：s = "cbbd"
+ * 输出："bb"
+ *
+ *
+ * 提示：
+ *
  * 1 <= s.length <= 1000
- * <p>
- * s consist of only digits and English letters.
+ * s 仅由数字和英文字母组成
+ * </pre>
  */
 public class _5_Longest_Palindromic_Substring extends LeetcodeProblemSolution {
 
-//    @Topic(TopicType.DP)
-//    @TimeComplexity(ComplexityType.O_N)
-//    @SpaceComplexity(ComplexityType.O_N)
-//    public String longestPalindrome(String s) {
-//        if (s == null || s.length() < 2) {
-//            return s;
-//        }
-//
-//        int maxLength = 1;
-//        int maxCenter = 0;
-//
-//        // make string length to be odd number
-//        // s consist of only digits and English letters, so can use '#'
-//        StringBuilder sb = new StringBuilder();
-//        sb.append("#");
-//        for (char c : s.toCharArray()) {
-//            sb.append(c).append("#");
-//        }
-//        String processed = sb.toString();
-//
-//        int n = processed.length();
-//        int[] dp = new int[n];
-//        int center = 0;
-//        int right = 0;
-//
-//        for (int i = 0; i < n; i++) {
-//            // use mirror of palindrome to save time
-//            if (i < right) {
-//                dp[i] = Math.min(dp[2 * center - i], right - i);
-//            }
-//
-//            while (i > dp[i] && i + dp[i] < n - 1 && processed.charAt(i - dp[i] - 1) == processed.charAt(i + dp[i] + 1)) {
-//                dp[i]++;
-//            }
-//
-//            // move the center and right if necessary
-//            if (i + dp[i] > right) {
-//                center = i;
-//                right = i + dp[i];
-//            }
-//
-//            if (dp[i] > maxLength) {
-//                maxLength = dp[i];
-//                maxCenter = i;
-//            }
-//        }
-//
-//        int realStart = (maxCenter - maxLength) / 2;
-//        return s.substring(realStart, realStart + maxLength);
-//    }
+    @Topic(TopicType.DP)
+    @TimeComplexity(ComplexityType.O_N)
+    @SpaceComplexity(ComplexityType.O_N)
+    public String longestPalindrome1(String s) {
+        if (s == null || s.length() < 2) {
+            return s;
+        }
+
+        int maxLength = 1;
+        int maxCenter = 0;
+
+        // make string length to be odd number
+        // s consist of only digits and English letters, so can use '#'
+        StringBuilder sb = new StringBuilder();
+        sb.append("#");
+        for (char c : s.toCharArray()) {
+            sb.append(c).append("#");
+        }
+        String processed = sb.toString();
+
+        int n = processed.length();
+        int[] dp = new int[n];
+        int center = 0;
+        int right = 0;
+
+        for (int i = 0; i < n; i++) {
+            // use mirror of palindrome to save time
+            if (i < right) {
+                dp[i] = Math.min(dp[2 * center - i], right - i);
+            }
+
+            while (i > dp[i] && i + dp[i] < n - 1 && processed.charAt(i - dp[i] - 1) == processed.charAt(i + dp[i] + 1)) {
+                dp[i]++;
+            }
+
+            // move the center and right if necessary
+            if (i + dp[i] > right) {
+                center = i;
+                right = i + dp[i];
+            }
+
+            if (dp[i] > maxLength) {
+                maxLength = dp[i];
+                maxCenter = i;
+            }
+        }
+
+        int realStart = (maxCenter - maxLength) / 2;
+        return s.substring(realStart, realStart + maxLength);
+    }
 
 
     @Topic(TopicType.TWO_POINTER)
