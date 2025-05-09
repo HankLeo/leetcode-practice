3c1e1aa01fb329c74b5ea1d412852e6605db0e4d
Hank Liu <hankleo92@gmail.com>
2025-05-09 12:04:33 +0800

    prac: java for problems _207_Course_Schedule

--- leetcode-practice-java/src/main/java/io/hank/leetcode/practices/graph/GraphPractices.java
+++ leetcode-practice-java/src/main/java/io/hank/leetcode/practices/graph/GraphPractices.java
@@ -3,7 +3,12 @@ package io.hank.leetcode.practices.graph;
 public class GraphPractices {
     public static void execute() {
         System.out.println("\n******** Graph ********");
+        new _207_Course_Schedule().execute();
         new _785_Is_Graph_Bipartite().execute();
         new _994_Rotting_Oranges().execute();
+    }
+
+    public static void main(String[] args) {
+        execute();
     }
 }
