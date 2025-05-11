6f1278325579538a7d15e5a7f2752c9eb8b2dc71
Hank Liu <hankleo92@gmail.com>
2025-05-11 11:23:35 +0800

    prac: java for problems _23_Merge_K_Sorted_Lists

--- leetcode-practice-java/src/main/java/io/hank/leetcode/practices/sort/SortPractices.java
+++ leetcode-practice-java/src/main/java/io/hank/leetcode/practices/sort/SortPractices.java
@@ -3,10 +3,15 @@ package io.hank.leetcode.practices.sort;
 public class SortPractices {
     public static void execute() {
         System.out.println("\n******** Sort *******");
+        new _23_Merge_K_Sorted_Lists().execute();
         new _215_Kth_Largest_Element_in_Array().execute();
         new _347_TopK_Frequent_Elements().execute();
         new _451_Sort_Characters_By_Frequency().execute();
         new _75_Sort_Colors().execute();
         new _148_Sort_List().execute();
+    }
+
+    public static void main(String[] args) {
+        execute();
     }
 }
