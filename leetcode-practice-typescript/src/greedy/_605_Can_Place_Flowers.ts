7c8b8c75fa40cd78542f7fdcf9efd71d00d53a34
Hao Liu <hliu4@opentext.com>
2025-05-15 15:49:42 +0800

    prac: ts for problems _605_Can_Place_Flowers.ts

---
+++ leetcode-practice-typescript/src/greedy/_605_Can_Place_Flowers.ts
@@ -0,0 +1,17 @@
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+    if (n === 0) {
+        return true;
+    }
+    for (let i = 0; i < flowerbed.length; i++) {
+        const afterZero: boolean = i === 0 || flowerbed[i - 1] === 0;
+        const beforeZero: boolean = i === flowerbed.length - 1 || flowerbed[i + 1] === 0;
+        if (flowerbed[i] === 0 && afterZero && beforeZero) {
+            n--;
+            flowerbed[i] = 1;
+            if (n === 0) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
