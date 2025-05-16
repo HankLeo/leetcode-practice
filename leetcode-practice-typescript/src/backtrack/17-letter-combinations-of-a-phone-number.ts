a026e2084a42ddcb960590471695a5e05f362ba5
Hank Liu <hankleo92@gmail.com>
2025-05-16 13:54:41 +0800

    prac: ts for problems 17-letter-combinations-of-a-phone-number.ts

---
+++ leetcode-practice-typescript/src/backtrack/17-letter-combinations-of-a-phone-number.ts
@@ -0,0 +1,31 @@
+function letterCombinations(digits: string): string[] {
+    const res: string[] = [];
+    const map: Record<string, string> = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz',
+    }
+
+    const backtrack = (index: number, path: string) => {
+        if (path.length === digits.length) {
+            res.push(path);
+            return;
+        }
+        for (let i = index; i < digits.length; i++) {
+            const letters = map[digits[index]];
+            for (let letter of letters) {
+                backtrack(i + 1, path + letter);
+            }
+        }
+    }
+
+    if (digits.length > 0) {
+        backtrack(0, '');
+    }
+    return res;
+};
