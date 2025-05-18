9c7ea7b571a3d0dbaf5e4e971e3675a643528b03
Hank Liu <hankleo92@gmail.com>
2025-05-19 00:38:08 +0800

    prac: ts for problems reverse-vowels-of-a-string

---
+++ leetcode-practice-typescript/src/two-pointers/345-reverse-vowels-of-a-string.ts
@@ -0,0 +1,20 @@
+function reverseVowels(s: string): string {
+    const targets = 'AEIOUaeiou';
+    const arr = s.split('');
+    let left = 0;
+    let right = s.length - 1;
+    while (left < right) {
+        if (!targets.includes(arr[left])) {
+            left++;
+            continue;
+        }
+        if (!targets.includes(arr[right])) {
+            right--;
+            continue;
+        }
+        [arr[left], arr[right]] = [arr[right], arr[left]];
+        left++;
+        right--;
+    }
+    return arr.join('');
+};
