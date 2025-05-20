69c99f316665126ca691c1cc2d38e123f698bd9d
Hao Liu <hliu4@opentext.com>
2025-05-20 11:03:37 +0800

    prac: ts for problems 328-odd-even-linked-list.ts

---
+++ leetcode-practice-typescript/src/linked-list/328-odd-even-linked-list.ts
@@ -0,0 +1,26 @@
+import {ListNode} from "../libs";
+
+function oddEvenList(head: ListNode | null): ListNode | null {
+    if (head == null) {
+        return head;
+    }
+    let curOdd = head;
+    let curEven = head.next;
+    let evenHead = curEven;
+    while (curOdd != null && curEven != null) {
+        let nextOdd = curEven.next;
+        if (nextOdd == null) {
+            break;
+        } else {
+            let nextEven = nextOdd.next;
+            curOdd.next = nextOdd;
+            curEven.next = nextEven;
+            curOdd = nextOdd;
+            curEven = nextEven;
+        }
+    }
+    curOdd.next = evenHead;
+    return head;
+}
+
+console.log(oddEvenList(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))))));
