01a3518f3b0f68129c10440b36f7e049f5c641b4
Hank Liu <hankleo92@gmail.com>
2025-06-07 17:42:06 +0800

    prac: python for problems 17-letter-combinations-of-a-phone-number.py

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ In this project, I have several implementations in different languages:
 
 - [Java](/leetcode-practice-java/src/main/java/io/hank/leetcode/practices)
 - [Kotlin](/leetcode-practice-kotlin/src/main/kotlin/io/hank/leetcode/kotlin/practices)
+- [Python](/leetcode-practice-python/src)
 - [TypeScript](/leetcode-practice-typescript/src)
---
+++ leetcode-practice-python/src/backtrack/17-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,57 @@
+import itertools
+
+
+class Solution:
+    def letter_combinations(self, digits: str) -> list[str]:
+        if not digits:
+            return list()
+
+        phone_map = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        def backtrack(index: int):
+            if index == len(digits):
+                result.append("".join(combination))
+            else:
+                digit = digits[index]
+                for letter in phone_map[digit]:
+                    combination.append(letter)
+                    backtrack(index + 1)
+                    combination.pop()
+
+        combination = list()
+        result = list()
+        backtrack(0)
+        return result
+
+    def letterCombinations(self, digits: str) -> list[str]:
+        if not digits:
+            return list()
+
+        phone_map = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        groups = (phone_map[digit] for digit in digits)
+        return ["".join(combination) for combination in itertools.product(*groups)]
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.letter_combinations("23"))
+    print(solution.letterCombinations("23"))
